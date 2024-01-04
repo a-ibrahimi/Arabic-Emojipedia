00dbb32ae8e3ef02f27f456faa6a356a7ea31dba
a-ibrahimi <a.ibrahimi@aui.ma>
2024-01-04 01:23:48 +0100

    Package name update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Arabic_Emojipedia',
+    name='arabic_emojipedia',
     version='1.0.0',
     packages=find_packages(),
     install_requires=[],
